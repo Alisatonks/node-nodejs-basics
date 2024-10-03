062c5244e7815892387c4df1265c78e1b3d0b43f
Volha Haiduk <Alisatonks2385@gmail.com>
2024-10-03 16:02:52 +0200

    feat: cli and modules

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    console.log('args', args);
+
+    const correctArgs = args.reduce((acc, curr, idx) => {
+
+        if(curr.startsWith('--')) {
+            acc.push(`${curr.slice(2)} is ${args[idx + 1]}`)
+        }
+       
+        return acc;
+    }, []);
+
+    const result = correctArgs.join(', ').trim();
+
+    console.log(result)
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envsAll = process.env;
+    
+    const envsRSS = Object.entries(envsAll).reduce((acc, [key, value]) => {
+        if (key.startsWith('RSS_')) {
+            acc += `${key}=${value}; `
+        }
+        return acc; 
+    }, '');
+
+    const result = envsRSS.trim()
+
+    console.log(result)
 };
 
 parseEnv();
